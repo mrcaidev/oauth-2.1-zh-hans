24c0f89aaf7e7f22ca0ecb5d16e56fe0f68d3db2
Yuwang Cai <mrcaidev@gmail.com>
2024-11-10 11:30:31 +0800

    chore: up-to-date vitepress config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   lang: "zh-Hans",
   cleanUrls: true,
   srcDir: "src",
+  metaChunk: true,
   lastUpdated: true,
   themeConfig: {
     logo: "/oauth.svg",
@@ -415,7 +416,7 @@ export default defineConfig({
     },
     editLink: {
       pattern:
-        "https://github.com/mrcaidev/oauth-2.1-zh-hans/edit/master/src/:path",
+        "https://github.com/mrcaidev/oauth-2.1-zh-hans/edit/main/src/:path",
       text: "在 GitHub 上编辑此页面",
     },
     lastUpdated: {
@@ -429,10 +430,13 @@ export default defineConfig({
       prev: "上一页",
       next: "下一页",
     },
-    darkModeSwitchLabel: "外观",
-    lightModeSwitchTitle: "切换到明亮外观",
-    darkModeSwitchTitle: "切换到暗黑外观",
+    darkModeSwitchLabel: "主题",
+    lightModeSwitchTitle: "切换到浅色模式",
+    darkModeSwitchTitle: "切换到深色模式",
     sidebarMenuLabel: "菜单",
     returnToTopLabel: "回到顶部",
+    search: {
+      provider: "local",
+    },
   },
 });
